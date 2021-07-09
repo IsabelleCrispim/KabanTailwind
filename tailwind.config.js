ad53f2f331e82eaec7a3d1fa30d2a0d7e9512f00
IsabelleCrispim <49999727+IsabelleCrispim@users.noreply.github.com>
2021-07-09 16:02:57 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkdark: '#1A2238',
+        blueBlue: '#9DAAF2',
+        orangeOrange: '#FF6A3D',
+        yellowyellow: '#F4DB7D',
+
+      },
+      height: {
+        'fit': 'fit-content'
+      },
+      width: {
+        'fit': 'fit-content'
+      },
+      maxHeight: {
+        '75': '75vh'
+      },
+    },
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
